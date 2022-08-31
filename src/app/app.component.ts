b3281c6e7f79c3078034e1cf563b9db1487c65ff
zlrlolo <1139940527@qq.com>
2022-08-31 23:57:01 +0800

    基本做完，还差移动端适配

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -20,7 +20,10 @@ export class AppComponent {
   mousemove1 = false;
   mousemove2 = false;
   btSearchtouch = false;
-  card = '写下你的问题，点击一个按钮'
+  card = '写下你的问题，点击搜索按钮'
+
+  @ViewChild('link')
+  linkElement!: ElementRef;
 
   ngOnInit() {
     let url = new URL(location.href)
@@ -28,7 +31,7 @@ export class AppComponent {
     if (userSearch !== null) {
       this.again = true;
       this.mousemove1 = true;
-      this.card='第一步，输入您的问题'
+      this.card = '第一步，输入一个问题'
       const begin = setTimeout(() => {
         this.searchbarTouch = true;
         const t = setInterval(() => {
@@ -39,31 +42,58 @@ export class AppComponent {
             clearInterval(t)
             this.mousemove1 = false;
             this.mousemove2 = true;
-            this.card='第二步，点击搜索按钮'
+            this.card = '第二步，点击搜索按钮'
             const w = setInterval(() => {
               this.btSearchtouch = true;
             }, 700);
             const l = setInterval(() => {
-              this.card='动动手，搜一下也不难嘛。'
+              this.card = '对你来说很难吗？'
             }, 2100);
+            const n = setInterval(() => {
+              const g_url = new URL("https://www.google.com/search");
+              g_url.searchParams.set('q', this.searchval)
+              console.log(g_url.href)
+              window.location.href=g_url.href
+              clearInterval(n)
+            }, 3100);
           }
         }, 300);
       }, 1500);
     }
   }
 
-  clicksearch() {
-
-  }
-
   sharelink() {
-    const url = new URL("http://localhost:4200");
-    url.searchParams.set('q', this.searchval)
-    this.newlink = url.href;
-    this.active = 1;
-    this.newsearch = this.newlink
-    this.card='复制或分享以下链接'
+    if (this.searchval) {
+      if (!this.again) {
+        const url = new URL(location.href);
+        url.searchParams.set('q', this.searchval)
+        this.newlink = url.href;
+        this.active = 1;
+        this.newsearch = this.newlink
+        this.card = '复制并分享给有需要的人'
+      }
+    }
   }
+
+  // /**
+  //  * @desc 复制纯文本到剪贴板
+  //  * @param word - 纯文本
+  //  */
+  copyToClip() {
+    const input: HTMLInputElement = this.linkElement.nativeElement
+    // const input = document.createElement('input')
+    // input.setAttribute('readonly', 'readonly')
+    // input.setAttribute('value', this.newlink)
+    // document.body.appendChild(input)
+    input.setSelectionRange(0, 9999)
+    input.select()
+    if (document.execCommand('Copy')) {
+      document.execCommand('Copy')
+      this.card = '已复制到剪贴板!'
+    }
+    // document.body.removeChild(input)
+  }
+
 
 }
 
